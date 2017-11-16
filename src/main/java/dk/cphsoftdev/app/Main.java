4a60210f09a7b61fe95cf29751ebc3e6c773b4a6
Ismail Cam <i@bigstep.dk>
2017-11-16 10:21:55 +0100

    Refactoring main method

--- src/main/java/dk/cphsoftdev/app/Main.java
+++ src/main/java/dk/cphsoftdev/app/Main.java
@@ -2,37 +2,23 @@ package dk.cphsoftdev.app;
 
 import dk.cphsoftdev.app.controller.Receiver;
 import dk.cphsoftdev.app.controller.Sender;
+import dk.cphsoftdev.app.controller.Validator;
 import dk.cphsoftdev.app.factory.Factory;
 
 public class Main
 {
     public static void main( String[] args )
     {
-        // Test only
-//        SenderController loanRequest = new SenderController( "group3.loanrequest.creditscore" );
-//        String request = "1209856785,120.0,36";
-//        if( new Validator().isValid( request ) )
-//        {
-//            Loan loan = new Loan( "1209856785,120.0,36" );
-//            loan.setCreditScore( 300 );
-//            loanRequest.sendMessage( loan );
-//        }
-
-        // Production
-        //Receiver receiver = new Receiver( "group3.loanrequest.creditscore" );
-        Sender sender = new Sender( "group3.creditscore.getbanks" );
-
-        String dummy = "1209852398,125000.0,12";
-        sender.sendMessage( new Factory().createLoan( dummy ) );
-
-        /*if( receiver.isReady() )
+        if( new Validator().isValid( args[ 0 ] ) )
         {
-            receiver.printMessages();
-            sender.sendMessage( new Factory().createLoan( receiver.getMessage() ) );
-        }*/
-
-        // Close connections
-        //receiver.close();
-        sender.close();
+            Sender sender = new Sender( "group3.creditscore.getbanks" );
+            sender.sendMessage( new Factory().createLoan( args[ 0 ] ) );
+            sender.close();
+        }
+        else
+        {
+            System.out.println( "Error! Invalid information. Please try again." );
+            System.exit( 1 );
+        }
     }
 }
